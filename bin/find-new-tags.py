1f7665c4e1c4907b2ac96080d42896601d6c8d0a
David Flanagan <david@rawkode.com>
2021-12-23 15:48:48 +0000

    feat: switch to doc.crds.dev for CRD Yaml, instead of Git clone

--- bin/find-new-tags.py
+++ bin/find-new-tags.py
@@ -2,15 +2,9 @@ from atoma import parse_atom_bytes
 from datetime import datetime, timedelta, timezone
 from json import dumps
 from requests import get as httpget
-from typing import List, Optional, TypedDict
+from typing import List, Mapping, Optional
 from yaml import safe_load, YAMLError
 import sys
-
-
-class Sdk(TypedDict):
-    name: str
-    repository: int
-    directory: str
 
 
 def has_been_updated(updated_at: Optional[datetime] = None) -> bool:
@@ -21,8 +15,8 @@ def has_been_updated(updated_at: Optional[datetime] = None) -> bool:
     return (datetime.now(timezone.utc) - updated_at) < timedelta(hours=hours_since)
 
 
-def get_new_tags(sdk: Sdk) -> List[str]:
-    response = httpget(f'{sdk["repository"]}/tags.atom')
+def get_new_tags(repository: str) -> List[str]:
+    response = httpget(f'https://{repository}/tags.atom')
     feed = parse_atom_bytes(response.content)
 
     return list(map(lambda entry: entry.title.value, filter(lambda entry: has_been_updated(entry.updated), feed.entries)))
@@ -30,18 +24,14 @@ def get_new_tags(sdk: Sdk) -> List[str]:
 
 with open("./sdks.yaml", "r") as stream:
     try:
-        # This doesn't actually enforce our schema,
-        # but I've decided to keep it in for now
-        # and will maybe drop in typeguard in the future
-        sdks: List[Sdk] = safe_load(stream)
+        sdks: Mapping[str, str] = safe_load(stream)
     except YAMLError as exc:
         print(f"Error parsing SDK Yaml: {exc}")
 
-
 sdks_to_build: List[str] = []
-for sdk in sdks:
-    for tag in get_new_tags(sdk):
+for name, repository in sdks.items():
+    for tag in get_new_tags(repository):
         sdks_to_build.append(
-            f'{sdk["name"]}|{sdk["repository"]}|{sdk["directory"]}|{tag}')
+            f'{name}|{repository}|{tag}')
 
 print(dumps(sdks_to_build))
